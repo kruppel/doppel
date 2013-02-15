4bc252838f6dc37bb077c0c35b00bc3c89dfb1bc
Kurt Ruppel <kurt.ruppel@gmail.com>
2013-02-15 12:17:05 -0800

    Disallows source directory to be in destination directory.

--- test/fixtures/actual/index.js
+++ test/fixtures/actual/index.js
@@ -3,6 +3,8 @@ var path = require('path');
 module.exports = {
 
   cwd_as_src: path.join(__dirname, 'cwd_as_src')
+, src_in_dest: path.join(__dirname, 'src_in_dest')
+
 , engines: require('./engines')
 
 };
